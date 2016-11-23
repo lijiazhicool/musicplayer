6c310daf5876df543a1710ece7b7730fa1133f4c
lijiazhi <jiazhi.li@mtime.com>
2016-11-23 13:15:35 +0800

    style

--- app/src/main/java/com/example/player/App.java
+++ app/src/main/java/com/example/player/App.java
@@ -1,5 +1,7 @@
 package com.example.player;
 
+import com.afollestad.appthemeengine.ATE;
+
 import android.app.Application;
 
 /**
@@ -7,4 +9,47 @@ import android.app.Application;
  */
 
 public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (!ATE.config(this, "light_theme").isConfigured()) {
+            ATE.config(this, "light_theme")
+                    .activityTheme(R.style.AppThemeLight)
+                    .primaryColorRes(R.color.colorPrimaryLightDefault)
+                    .accentColorRes(R.color.colorAccentLightDefault)
+                    .coloredNavigationBar(false)
+                    .usingMaterialDialogs(true)
+                    .commit();
+        }
+        if (!ATE.config(this, "dark_theme").isConfigured()) {
+            ATE.config(this, "dark_theme")
+                    .activityTheme(R.style.AppThemeDark)
+                    .primaryColorRes(R.color.colorPrimaryDarkDefault)
+                    .accentColorRes(R.color.colorAccentDarkDefault)
+                    .coloredNavigationBar(false)
+                    .usingMaterialDialogs(true)
+                    .commit();
+        }
+        if (!ATE.config(this, "light_theme_notoolbar").isConfigured()) {
+            ATE.config(this, "light_theme_notoolbar")
+                    .activityTheme(R.style.AppThemeLight)
+                    .coloredActionBar(false)
+                    .primaryColorRes(R.color.colorPrimaryLightDefault)
+                    .accentColorRes(R.color.colorAccentLightDefault)
+                    .coloredNavigationBar(false)
+                    .usingMaterialDialogs(true)
+                    .commit();
+        }
+        if (!ATE.config(this, "dark_theme_notoolbar").isConfigured()) {
+            ATE.config(this, "dark_theme_notoolbar")
+                    .activityTheme(R.style.AppThemeDark)
+                    .coloredActionBar(false)
+                    .primaryColorRes(R.color.colorPrimaryDarkDefault)
+                    .accentColorRes(R.color.colorAccentDarkDefault)
+                    .coloredNavigationBar(true)
+                    .usingMaterialDialogs(true)
+                    .commit();
+        }
+
+    }
 }
